a4d3f1593a105e3d7955fc615c803b7498830887
wo874 <92176181+wo874@users.noreply.github.com>
2024-03-01 23:23:06 -0500

    Update IntelliCheck.py

--- IntelliCheck.py
+++ IntelliCheck.py
@@ -1,15 +1,19 @@
+from spellchecker import SpellChecker
 import spacy
 
 def context_aware_spell_check(text):
     nlp = spacy.load('en_core_web_sm')
+    spell = SpellChecker()
+
     doc = nlp(text)
 
     corrected_text = ""
     for token in doc:
         if token.is_alpha and not token.is_stop:
-            corrected_text += token.text + " "
+            corrected_token = spell.correction(token.text)
+            corrected_text += corrected_token + " "
         else:
-            corrected_text += token._.suggestions[0] + " " if token._.has("suggestions") else token.text + " "
+            corrected_text += token.text + " "
 
     return corrected_text.strip()
 
@@ -22,3 +26,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
