9cc5e17ad11b25eef2545af8ee0e8326870751f8
wo874 <92176181+wo874@users.noreply.github.com>
2024-03-01 23:14:05 -0500

    Update IntelliCheck.py

--- IntelliCheck.py
+++ IntelliCheck.py
@@ -1,19 +1,8 @@
-from spellchecker import SpellChecker
+from autocorrect import Speller
 
 def spell_check(text):
-    spell = SpellChecker()
-    words = text.split()
-
-    misspelled = spell.unknown(words)
-
-    corrected_text = ""
-    for word in words:
-        if word in misspelled:
-            corrected_text += spell.correction(word) + " "
-        else:
-            corrected_text += word + " "
-
-    return corrected_text.strip()
+    spell = Speller(lang='en')
+    return spell(text)
 
 def main():
     input_text = input("Enter text to spell-check: ")
